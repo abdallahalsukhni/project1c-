5a4b4d36499d8230d418d5ead9d1c7b29696f8cb
abdallahalsukhni <abdullahalsukhni@gmail.com>
2022-09-26 21:24:39 -0500

    first changes

---
+++ project1_tk22994_aha2242.cpp
@@ -0,0 +1,93 @@
+/* 
+// File Name: project1_tk22994.aha2242.cpp
+// Authors: Tushar Kohli, Abdallah Al-Sukhni
+// Date: 25 Sep 2022
+// Assignment Number 1
+// CS 105C Spring 2022
+// Instructor: Dr. Palacios
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+
+// C++ program that will display a list of the 
+// top Internet stories on Instagram
+struct Story{
+    int score;
+    string title;
+    string url;
+};
+
+
+int main() {
+    string fileToBeOpened;
+    cout << "type the name of the file you would like to open: ";
+    cin >> fileToBeOpened;
+    ifstream myfile; 
+    myfile.open(fileToBeOpened);
+    int storyArrSize;
+    myfile >> storyArrSize;
+    cout << storyArrSize << endl;
+    Story *storyArr = nullptr;
+    storyArr = new Story[storyArrSize];
+    // read input data into array
+    for(int storyNum = 0; storyNum < storyArrSize; storyNum++) {
+        string url;
+        string title;
+        int score;
+        std::getline(myfile, title);
+        std::getline(myfile, url);
+        myfile >> score;
+        cout << score << endl;
+        cout << url << endl;
+        cout << title << endl;
+        Story newStory = {score, title, url};
+        *(storyArr + storyNum) = newStory;
+    }
+    // test to see if correct values in array(this is only for testing)
+    for(int i = 0; i < storyArrSize; i++) {
+        cout << storyArr[i].score << " " << storyArr[i].title << endl;
+    }
+
+    // find mode, map int to vector of story
+    // for each score, have a list of vectors with that score
+    unordered_map<int, vector<Story>> map;
+    for(int i = 0; i < storyArrSize; i++) {
+        Story story = *(storyArr + i);
+        if(map.count(story.score) == 0) {
+            vector<Story> thisScoreVector;
+            thisScoreVector.push_back(story);
+            map[story.score] = thisScoreVector
+        } else {
+            map[story.score].push_back(story);
+        }
+    }
+    vector<Story> modeVector;
+    int currFreq = -1;
+    bool foundDiff = false;
+    // then iterate through map and find frequency with vector size that is the highest, that will be our mode frequency
+    // no mode if all frequencies are the same(return -1)
+    // have a vector that points to the current highest frequency's story vector, and return those stats
+    for(int freq : map) {
+        if(currFreq == -1) {
+            currFreq = freq;
+        } else if(currFreq != freq) {
+            foundDiff = true;
+            break;
+        }
+    }
+    if(!foundDiff) {
+        cout << -1;
+    }
+
+    // finally, print out mode, and each story's title and url in the final mode vector
+    return 0;
+}
+
+
