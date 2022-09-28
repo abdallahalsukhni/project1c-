6ee4c0cd68d7d4a94b2af0d2d20d97f7ad56c792
Tushar K <kohli.tushar18@gmail.com>
2022-09-28 17:06:05 -0500

    eh

--- project1_tk22994_aha2242.cpp
+++ project1_tk22994_aha2242.cpp
@@ -15,7 +15,6 @@
 #include <vector>
 using namespace std;
 
-
 // C++ program that will display a list of the 
 // top Internet stories on Instagram
 struct Story {
@@ -23,6 +22,8 @@ struct Story {
   string title;
   string url;
 };
+
+int findMode(const int* STORY_ARR, const int STORY_ARR_SIZE);
 
 int main() {
   string fileToBeOpened;
@@ -53,11 +54,23 @@ int main() {
     *(storyArr + storyNum) = newStory;
   }
 
+  int trueMode = findMode(storyArr, storyArrSize);
+  // finally, print out mode, and each story's title and url in the final mode vector
+  cout << "Mode: " << trueMode << "\n\n";
+  vector<Story> storiesToBePrinted = map[trueMode];
+  for(auto& thisStory : storiesToBePrinted) {
+    cout << thisStory.title << "\n" << thisStory.url << "\n\n";
+  }
+
+  return 0;
+}
+
+int findMode(const int* STORY_ARR, const int STORY_ARR_SIZE) {
   // find mode, map int to vector of story
   // for each score, have a vector of stories with that score
   unordered_map<int, vector<Story> > map;
-  for (int i = 0; i < storyArrSize; i++) {
-    Story story = *(storyArr + i);
+  for (int i = 0; i < STORY_ARR_SIZE; i++) {
+    Story story = *(STORY_ARR + i);
     if (map.count(story.score) == 0) {
       vector<Story> thisScoreVector;
       thisScoreVector.push_back(story);
@@ -70,7 +83,7 @@ int main() {
   // check to see if all modes are the same or not
   int currFreq = -1;
   bool foundDiff = false;
-  for(auto& storyFreq : map) {
+  for (auto& storyFreq : map) {
     if(currFreq == -1) {
       currFreq = storyFreq.second.size();
     } else if(currFreq != storyFreq.second.size()) {
@@ -78,12 +91,14 @@ int main() {
       break;
     }
   }
-  if(!foundDiff)
+  if (!foundDiff)
     cout << -1;
 	
   vector<int> modeVector;
   int mode = 0;
   int mostFreqSoFar = 0;
+
+
   // calculate what the mode is
   for(auto& storyFreq : map) {
     if(storyFreq.second.size() > mostFreqSoFar) {
@@ -93,19 +108,19 @@ int main() {
   }
 
   // if there are multiple modes then add each of them to a list, and find which one occurs first in input file
-  for(auto& storyFreq : map) {
+  for (auto& storyFreq : map) {
     if(storyFreq.second.size() == mostFreqSoFar) {
       modeVector.push_back(storyFreq.first);
     }
   }
   int trueMode = -1;
   // find which mode occurs first in input file
-  for(int spot = 0; spot < storyArrSize; spot++) {
+  for(int spot = 0; spot < STORY_ARR_SIZE; spot++) {
     bool found = false;
-    Story story = *(storyArr + spot);
+    Story story = *(STORY_ARR + spot);
     int modeToBeFound = story.score;
-    for(auto& thisMode : modeVector) {
-      if(thisMode == modeToBeFound) {
+    for (auto& thisMode : modeVector) {
+      if (thisMode == modeToBeFound) {
         found = true;
         trueMode = thisMode;
         break;
@@ -116,13 +131,6 @@ int main() {
     }
   }
 
-  // finally, print out mode, and each story's title and url in the final mode vector
-  cout << "Mode: " << trueMode << "\n\n";
-  vector<Story> storiesToBePrinted = map[trueMode];
-  for(auto& thisStory : storiesToBePrinted) {
-    cout << thisStory.title << "\n" << thisStory.url << "\n\n";
-  }
-  return 0;
+  return trueMode;
 }
-
 
